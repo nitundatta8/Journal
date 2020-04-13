68b596bd7f939bfbe82a2dfa653446660a13afd7
nitunwa <nitun.2@gmail.com>
2020-04-13 14:50:05 -0700

    add function to get numbers of vowels and consonants

--- src/journal.js
+++ src/journal.js
@@ -3,10 +3,27 @@ export function Entry(title,body){
   this.body = body;
 }
 
-//var entry = new Entry("Rugs", "Rugs can be made of wool, silk, cotton, jute, or synthetic fibers.");
 Entry.prototype.countWords = function(){
   var str =this.body;
   var strArray = str.split(" ");
   return strArray.length;
 };
 
+Entry.prototype.CountLetters = function() {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  var str = this.body;
+  var characterString = str.split("");
+  var i = 0;
+  var vowelsInString = [];
+  var consonantsInString = [];
+  for (characterString[i]=0; i<characterString.length; i++) {
+    if (vowels.includes(characterString[i])) {
+      vowelsInString.push(characterString[i]);  
+    }
+    else if (consonants.includes(characterString[i])) {
+      consonantsInString.push(characterString[i]); 
+    }
+  }
+  return  "Vowels In String: "+ vowelsInString.length +" Consonants In String: " + consonantsInString.length;
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { Entry } from './journal';
+import { Entry } from './journal.js';
 import './styles.css';
 
 $(document).ready(function() {
-  
+  var entry = new Entry("Rugs", "Rugs can be made of wool, silk, cotton, jute, or synthetic fibers.");
+  var value = entry.countWords(); 
+  console.log(value);
+  var letters = entry.CountLetters();
+  console.log(letters);
 });
+
